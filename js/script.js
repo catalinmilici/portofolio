3d81bc02db6be0210066e6186935f3cd904fd519
Catalin Milici <catalin@ecomdigital.ro>
2023-03-09 09:18:05 +0200

    projects section

--- js/script.js
+++ js/script.js
@@ -21,3 +21,42 @@ const words = ["Awesome Design", "Sematic HTML", "Landing Pages"];
 let i = 0,
   timer;
 typingEffect();
+
+//Filter category case studies
+const filterContainer = document.querySelector(".project-filter"),
+  Items = document.querySelectorAll(".project-item");
+if (filterContainer) {
+    filterContainer.addEventListener("click", (event) => {
+        if (event.target.classList.contains("filter-item")) {
+            filterContainer.querySelector(".active").classList.remove("active");
+            event.target.classList.add("active");
+            const filterValue = event.target.getAttribute("data-filter");
+            Items.forEach((item) => {
+                if (item.classList.contains(filterValue) || filterValue === 'all') {
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                }
+                else {
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            });
+        }
+    });
+}
+
+const progressContainer = document.querySelector('.progress-container');
+
+// initial call
+setPercentage();
+
+function setPercentage() {
+  const percentage = progressContainer.getAttribute('data-percentage') + '%';
+  
+  const progressEl = progressContainer.querySelector('.progress');
+  const percentageEl = progressContainer.querySelector('.percentage');
+  
+  progressEl.style.width = percentage;
+  percentageEl.innerText = percentage;
+  percentageEl.style.left = percentage;
+}
