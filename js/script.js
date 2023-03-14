122f8357ee8448b2044355785dddee393a1ab401
Catalin Milici <catalin@ecomdigital.ro>
2023-03-14 23:44:13 +0200

    light theme and switch buton to light/dark

--- js/script.js
+++ js/script.js
@@ -71,3 +71,21 @@ window.onscroll = function () {
     }
     prevScrollpos = currentScrollPos;
 }
+
+
+// change theme style
+function toggleTheme() {
+  // Obtains an array of all <link>
+  // elements.
+  // Select your element using indexing.
+  var theme = document.getElementsByTagName("link")[1];
+  const indicator = document.querySelector(".indicator");
+    indicator.classList.toggle("change");
+  // Change the value of href attribute
+  // to change the css sheet.
+  if (theme.getAttribute("href") == "/css/light.css") {
+    theme.setAttribute("href", "/css/dark.css");
+  } else {
+    theme.setAttribute("href", "/css/light.css");
+  }
+}
