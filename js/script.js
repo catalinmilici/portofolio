301e2b8bf3f04c645d3e1c0d7fcd0a38777c97be
Catalin Milici <catalin@ecomdigital.ro>
2023-03-29 23:17:10 +0300

    altele

--- js/script.js
+++ js/script.js
@@ -139,3 +139,68 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+
+	
+		//Scroll back to top
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);	
+		var offset = 50;
+		var duration = 550;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.progress-wrap').addClass('active-progress');
+			} else {
+				jQuery('.progress-wrap').removeClass('active-progress');
+			}
+		});				
+		jQuery('.progress-wrap').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+		
+		$(window).on("scroll", function () {
+      let scroll = $(window).scrollTop();
+      let oTop = $(".skill-progress-bar").offset().top - window.innerHeight;
+      if (scroll > oTop) {
+        $(".progress").addClass("progressbar-active");
+      } else {
+        $(".progress").removeClass("progressbar-active");
+      }
+    });
+
+ $(function () {
+   $('a[href^="#"]').click(function () {
+     if (
+       location.pathname.replace(/^\//, "") ==
+         this.pathname.replace(/^\//, "") &&
+       location.hostname == this.hostname
+     ) {
+       var target = $(this.hash);
+       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+       if (target.length) {
+         $("html,body").animate(
+           {
+             scrollTop: target.offset().top,
+           },
+           1000
+         );
+         return false;
+       }
+     }
+   });
+ });
